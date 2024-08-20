9ed382b27341d7d8c68e627f6497d947a5969acc
hefraimjr <zfollen888@gmail.com>
2024-08-20 17:31:43 -0400

    Mirella do arburgui passado

---
+++ src/Oferta/index.jsx
@@ -0,0 +1,11 @@
+
+import Header from '../components/Header';
+export default function Oferta(){
+    return(
+        <div>
+            <Header/>
+            <h1>Oferta</h1>
+        </div>
+        
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Oferta from './Oferta';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+         <Route path="/" element={<Home/>}/>    
+         <Route path="Oferta" element={<Oferta/>}/> 
+        </Routes>      
+      </BrowserRouter>
+    </React.StrictMode>,
 )
-
-
-
